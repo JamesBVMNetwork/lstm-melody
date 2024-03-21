a2a447f4f7795a3bd153669331bda8980decb0c1
JamesNBC <1026@newbitcoincity.com>
2024-03-21 16:37:00 +0700

    add base

---
+++ train_script.py
@@ -0,0 +1,405 @@
+from mido import MidiFile, MidiTrack, Message
+import mido
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import LSTM
+from keras.layers import Embedding
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.utils import np_utils, to_categorical
+from keras.models import load_model
+import os
+from tqdm import *
+
+import pygame
+# import IPython
+import matplotlib.pyplot as plt
+# import librosa.display
+# from IPython import *
+from music21 import *
+from music21 import converter, instrument, note, chord, stream, midi
+import glob
+import time
+import numpy as np
+import keras.utils as utils
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow import distribute
+import struct
+import base64
+import json
+import site
+import argparse
+
+strategy = distribute.MirroredStrategy(["GPU:0", "GPU:1"])
+
+# Melody-RNN Format is a sequence of 8-bit integers indicating the following:
+# MELODY_NOTE_ON = [0, 127] # (note on at that MIDI pitch)
+MELODY_NOTE_OFF = 128  # (stop playing all previous notes)
+MELODY_NO_EVENT = 129  # (no change from previous event)
+# Each element in the sequence lasts for one sixteenth note.
+# This can encode monophonic music only.
+VOCAB_SIZE = 130
+
+def parse_args():
+    parser = argparse.ArgumentParser("Entry script to launch training")
+    parser.add_argument("--data-dir", type=str, default = "./data", help="Path to the data directory")
+    parser.add_argument("--output-dir", type=str, default = "./outputs", help = "Path to output directory")
+    parser.add_argument("--config-path", type=str, required = True, help="Path to the output file")
+    parser.add_argument("--checkpoint-path", type = str, default = None,  help="Path to the checkpoint file")
+    return parser.parse_args()
+
+def streamToNoteArray(stream):
+    """
+    Convert a Music21 sequence to a numpy array of int8s into Melody-RNN format:
+        0-127 - note on at specified pitch
+        128   - note off
+        129   - no event
+    """
+    # Part one, extract from stream
+    total_length = np.int(
+        np.round(stream.flat.highestTime / 0.25))  # in semiquavers
+    stream_list = []
+    for element in stream.flat:
+        if isinstance(element, note.Note):
+            stream_list.append([np.round(
+                element.offset / 0.25), np.round(element.quarterLength / 0.25), element.pitch.midi])
+        elif isinstance(element, chord.Chord):
+            stream_list.append([np.round(element.offset / 0.25), np.round(
+                element.quarterLength / 0.25), element.sortAscending().pitches[-1].midi])
+    np_stream_list = np.array(stream_list, dtype=np.int)
+    df = pd.DataFrame(
+        {'pos': np_stream_list.T[0], 'dur': np_stream_list.T[1], 'pitch': np_stream_list.T[2]})
+    # sort the dataframe properly
+    df = df.sort_values(['pos', 'pitch'], ascending=[True, False])
+    df = df.drop_duplicates(subset=['pos'])  # drop duplicate values
+    # part 2, convert into a sequence of note events
+    # set array full of no events by default.
+    output = np.zeros(total_length+1, dtype=np.int16) + \
+        np.int16(MELODY_NO_EVENT)
+    # Fill in the output list
+    for i in range(total_length):
+        if not df[df.pos == i].empty:
+            # pick the highest pitch at each semiquaver
+            n = df[df.pos == i].iloc[0]
+            output[i] = n.pitch  # set note on
+            output[i+n.dur] = MELODY_NOTE_OFF
+    return output
+
+def noteArrayToDataFrame(note_array):
+    """
+    Convert a numpy array containing a Melody-RNN sequence into a dataframe.
+    """
+    df = pd.DataFrame({"code": note_array})
+    df['offset'] = df.index
+    df['duration'] = df.index
+    df = df[df.code != MELODY_NO_EVENT]
+    # calculate durations and change to quarter note fractions
+    df.duration = df.duration.diff(-1) * -1 * 0.25
+    df = df.fillna(0.25)
+    return df[['code', 'duration']]
+
+def noteArrayToStream(note_array):
+    """
+    Convert a numpy array containing a Melody-RNN sequence into a music21 stream.
+    """
+    df = noteArrayToDataFrame(note_array)
+    melody_stream = stream.Stream()
+    for index, row in df.iterrows():
+        if row.code == MELODY_NO_EVENT:
+            # bit of an oversimplification, doesn't produce long notes.
+            new_note = note.Rest()
+        elif row.code == MELODY_NOTE_OFF:
+            new_note = note.Rest()
+        else:
+            new_note = note.Note(row.code)
+        new_note.quarterLength = row.duration
+        melody_stream.append(new_note)
+    return melody_stream
+
+# making data to train
+def make_training_data(dirname):
+    note_on = []
+    for currentpath,_, files in os.walk(dirname):
+        for file in files:
+            path = os.path.join(currentpath, file)
+            if path.endswith('.mid') or path.endswith('.midi'):
+                mid = MidiFile(path)
+                for j in range(len(mid.tracks)):
+                    for i in mid.tracks[j]:
+                        if str(type(i)) != "<class 'mido.midifiles.meta.MetaMessage'>":
+                            x = str(i).split(' ')
+                            if x[0] == 'note_on':
+                                note_on.append(int(x[2].split('=')[1]))
+
+    # making data to train
+    training_data = []
+    labels = []
+    for i in range(20, len(note_on)):
+        inputs = note_on[i-20:i]
+        # inputs = to_categorical(inputs, num_classes=VOCAB_SIZE)
+        training_data.append(inputs)
+        targets = [note_on[i]]
+        targets = to_categorical(targets, num_classes=VOCAB_SIZE)
+        labels.append(targets)
+    return training_data,labels
+
+def create_model(num_units, ckpt = None):
+    if ckpt is not None:
+        model = tf.keras.models.load_model(ckpt)
+        return model
+    
+    model = Sequential()
+
+    model.add(LSTM(num_units, input_shape=(20, 1), unroll=True,
+                return_sequences=True, implementation=1))
+    model.add(Dropout(0.4))
+    # model.add(LSTM(num_units // 2, return_sequences=True, implementation=1))
+    # model.add(Dense(num_units // 2, 'relu'))
+    # model.add(Dropout(0.2))
+    model.add(Dense(VOCAB_SIZE, 'softmax'))
+
+    model.compile(loss='MSE', optimizer='adam') 
+    model.summary()
+
+    return model
+
+def train(training_data, labels, num_units=128, num_epochs=20, loss='MSE', optimizer='adam', early_stop=True, output_dir = './outputs', checkpoint_path = None):
+    num_units = int(num_units)
+    num_epochs = int(num_epochs)
+    early_stop = early_stop == 'True'
+
+    model = create_model(num_units, checkpoint_path)
+
+    early_stop_cb = EarlyStopping(
+        monitor='val_loss', patience=20, verbose=0)
+
+    training_data = np.array(training_data)
+    training_data = training_data.reshape(
+        (training_data.shape[0], training_data.shape[1], 1))
+    labels = np.array(labels)
+
+    # train
+    X_train, X_test, y_train, y_test = train_test_split(
+        training_data, labels, test_size=0.05, random_state=42)
+    model.fit(X_train, y_train, epochs=num_epochs, batch_size=32 * strategy.num_replicas_in_sync,
+              validation_data=(X_test, y_test), callbacks=([early_stop_cb] if early_stop else []))
+    output_checkpoint_file = os.path.join(output_dir, 'model.h5')
+    output_file = os.path.join(output_dir, 'model.json')
+    model.save(output_checkpoint_file)
+    get_model_for_export(output_file, model)
+
+def get_weights(model):
+    weights = []
+    for layer in model.layers:
+        w = layer.get_weights()
+        print(layer.name)
+        print([g.shape for g in w])
+        weights.append(w)
+    return weights
+
+def compressConfig(data):
+    layers = []
+    for layer in data["config"]["layers"]:
+        cfg = layer["config"]
+        if layer["class_name"] == "InputLayer":
+            layer_config = {
+                "batch_input_shape": cfg["batch_input_shape"]
+            }
+        elif layer["class_name"] == "Rescaling":
+            layer_config = {
+                "scale": cfg["scale"],
+                "offset": cfg["offset"]
+            }
+        elif layer["class_name"] == "Dense":
+            layer_config = {
+                "units": cfg["units"],
+                "activation": cfg["activation"]
+            }
+        elif layer["class_name"] == "Conv2D":
+            layer_config = {
+                "filters": cfg["filters"],
+                "kernel_size": cfg["kernel_size"],
+                "strides": cfg["strides"],
+                "activation": cfg["activation"],
+                "padding": cfg["padding"]
+            }
+        elif layer["class_name"] == "MaxPooling2D":
+            layer_config = {
+                "pool_size": cfg["pool_size"],
+                "strides": cfg["strides"],
+                "padding": cfg["padding"]
+            }
+        elif layer["class_name"] == "Embedding":
+            layer_config = {
+                "input_dim": cfg["input_dim"],
+                "output_dim": cfg["output_dim"]
+            }
+        elif layer["class_name"] == "SimpleRNN":
+            layer_config = {
+                "units": cfg["units"],
+                "activation": cfg["activation"]
+            }
+        elif layer["class_name"] == "LSTM":
+            layer_config = {
+                "units": cfg["units"],
+                "activation": cfg["activation"],
+                "recurrent_activation": cfg["recurrent_activation"],
+            }
+        else:
+            layer_config = None
+
+        res_layer = {
+            "class_name": layer["class_name"],
+        }
+        if layer_config is not None:
+            res_layer["config"] = layer_config
+        layers.append(res_layer)
+
+    return {
+        "config": {
+            "layers": layers
+        }
+    }
+
+def get_model_for_export(fname, model):
+    weight_np = get_weights(model)
+
+    weight_bytes = bytearray()
+    for idx, layer in enumerate(weight_np):
+        # print(layer.shape)
+        # write_to_file(f"model_weight_{idx:02}.txt", str(layer))
+        for weight_group in layer:
+            flatten = weight_group.reshape(-1).tolist()
+            # print("flattened length: ", len(flatten))
+            for i in flatten:
+                weight_bytes.extend(struct.pack("@f", float(i)))
+
+    weight_base64 = base64.b64encode(weight_bytes).decode()
+    config = json.loads(model.to_json())
+    # print("full config: ", config)
+
+    compressed_config = compressConfig(config)
+    # write to file
+    with open(fname, "w") as f:
+        json.dump({
+            "model_name": "musicnetgen",
+            "layers_config": compressed_config,
+            "weight_b64": weight_base64,
+        }, f)
+    return weight_base64, compressed_config
+
+# prediction
+# def _sample_with_temperature(probabilites, temperature):
+#     """Samples an index from a probability array reapplying softmax using temperature
+
+#     :param predictions (nd.array): Array containing probabilities for each of the possible outputs.
+#     :param temperature (float): Float in interval [0, 1]. Numbers closer to 0 make the model more deterministic.
+#         A number closer to 1 makes the generation more unpredictable.
+
+#     :return index (int): Selected output symbol
+#     """
+#     predictions = np.log(probabilites) / temperature
+#     probabilites = np.exp(predictions) / np.sum(np.exp(predictions))
+
+#     choices = range(len(probabilites))
+#     index = np.random.choice(choices, p=probabilites)
+
+#     return index
+
+# def generate_melody(training_data,model):
+#     model=load_trained_model(output)
+
+#     n = 100
+#     # randomize the starter notes
+#     index = np.random.choice(len(training_data))
+#     starter_notes = np.array(training_data[index])
+#     x = starter_notes.reshape(1, 20, 1)
+#     # print("starter: ", x.shape)
+#     tune = list(starter_notes.reshape(-1,))
+#     for i in range(n):
+#         out = model.predict(x)[:, 0:1, :]
+#         # print("output shape: ", out.shape)
+#         # pred = int(out[0][out.shape[1]-1][0])
+#         next_note = out[0][0]
+#         pred = _sample_with_temperature(next_note, 0.5)
+#         # if round(pred) == round(tune[-1]):
+#         #     p = np.random.choice(['a', 'b', 'c'])
+#         #     if p == 'a':
+#         #         pred = 65
+#         #     elif p == 'b':
+#         #         pred = 60
+#         #     else:
+#         #         pred = 70
+#         tune.append(pred)
+#         # add out to x
+#         vec = np.zeros((1, 1, 1))
+#         vec[0][0][0] = pred
+#         x = np.append(x, vec, axis=1)
+#         x = x[:, -20:, :]
+#         # print("x shape: ", x.shape)
+#         # x = x.reshape(1, inputlen, VOCAB_SIZE)
+
+#     tune = list(np.array(tune).astype('float32'))
+#     print("generated melody", tune)
+
+#     # encoder
+
+#     offset = 0
+
+#     output_notes = []
+#     output_melody_stream = stream.Stream()
+#     # create note and chord objects based on the values generated by the model
+#     for patterns in tune:
+#         pattern = str(patterns)
+#         # pattern is a chord
+#         if ('.' in pattern) or pattern.isdigit():
+#             notes_in_chord = pattern.split('.')
+#             notes = []
+#             for current_note in notes_in_chord:
+#                 new_note = note.Note(int(current_note))
+#                 new_note.storedInstrument = instrument.Piano()
+#                 notes.append(new_note)
+#             new_chord = chord.Chord(notes)
+#             new_chord.offset = offset
+#             output_notes.append(new_chord)
+#             output_melody_stream.append(new_chord)
+#         # pattern is a note
+#         else:
+#             new_note = note.Note(pattern)
+#             new_note.offset = offset
+#             new_note.storedInstrument = instrument.Piano()
+#             output_notes.append(new_note)
+#             output_melody_stream.append(new_note)
+#         # increase offset each iteration so that notes do not stack
+#         offset += 0.5
+
+#     # write to midi file
+#     output_melody_stream.write('midi', fp='musicnetgen.mid')
+#     # print(output_melody_stream)
+
+#     # print(output_melody_stream)
+#     # output_melody_stream.show()
+
+def main():
+    args = parse_args()
+
+    data_dir = args.data_dir
+    output_dir = args.output_dir
+    ckpt = args.checkpoint_path
+    config_path = args.config_path
+
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+    num_units = config["rnn_units"]
+    num_epochs = config["epoch_num"]
+
+    training_data,labels=None,None
+    training_data,labels=make_training_data(data_dir)
+    
+
+    train(training_data,labels,num_units,num_epochs,'MSE','adam', True, output_dir, ckpt)
+
+if __name__ == "__main__":
+    main()
