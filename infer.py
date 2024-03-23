15d811320e23c873a3ab032e88029ecac6665ada
JamesNBC <1026@newbitcoincity.com>
2024-03-23 15:10:29 +0700

    add infer

--- infer.py
+++ infer.py
@@ -29,16 +29,13 @@ def generate_melody(input_notes, model, seq_length = 100, to_generate = 100):
     input_notes = input_notes[-seq_length: ]
     if len(input_notes) < seq_length:
         input_notes = [MELODY_NO_EVENT for _ in range(seq_length - len(input_notes))] + input_notes
+    prediction_output = []
     for i in range(to_generate):
-        input_notes = np.array(input_notes)/float(MELODY_SIZE)  
-        input_notes = input_notes.reshape(1, seq_length, 1)
-        prediction = model.predict(input_notes)
-        index = np.argmax(prediction)
-        input_notes.append(index)
-        input_notes = input_notes[1:]
-
-
-
+        prediction_input = np.array(input_notes).reshape(1, seq_length)
+        prediction = model.predict(prediction_input)
+        prediction = np.argmax(prediction, axis=1)
+        prediction_output.append(prediction[0])
+        input_notes = input_notes[1:] + [prediction[0]]
 
     return prediction_output
 
@@ -48,4 +45,6 @@ if __name__ == '__main__':
     checkpoint_path = args.checkpoint_path
     model = load_model_from_ckpt(checkpoint_path)
     model.summary()
-    generate()
+    input_notes = [MELODY_NO_EVENT for _ in range(100)]
+    melody = generate_melody(input_notes, model)
+    print(melody)
