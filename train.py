b24d36846929b4c7f936015473ba4394b99b56ea
JamesNBC <1026@newbitcoincity.com>
2024-03-24 12:04:19 +0700

    change config

--- train.py
+++ train.py
@@ -167,6 +167,7 @@ def create_model(config, model_path = None):
         tf.keras.layers.InputLayer(input_shape=(sequence_length,)),
         tf.keras.layers.Embedding(input_dim=MELODY_SIZE, output_dim=embedding_dim, input_length=sequence_length),
         tf.keras.layers.LSTM(units = rnn_units, return_sequences=True),
+        tf.keras.layers.LSTM(units = rnn_units, return_sequences=True),
         tf.keras.layers.LSTM(units = rnn_units),
         tf.keras.layers.Dropout(0.2),
         tf.keras.layers.Dense(n_vocab)
