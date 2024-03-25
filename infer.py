ade86d423b5c74012c155e9da439cbef763d1638
rein-nbc <rein1605@newbitcoincity.com>
2024-03-25 16:31:31 +0700

    Add layer debug

--- infer.py
+++ infer.py
@@ -5,6 +5,9 @@ import os
 import json
 from music21 import instrument, note, stream
 import pandas as pd
+from keract import get_activations
+
+np.set_printoptions(threshold=np.inf)
 
 # Melody-RNN Format is a sequence of 8-bit integers indicating the following:
 # MELODY_NOTE_ON = [0, 127] # (note on at that MIDI pitch)
@@ -15,6 +18,10 @@ MELODY_NO_EVENT = 129  # (no change from previous event)
 MELODY_SIZE = 130
 
 SEQUENCE_LENGTH = 40
+
+def write_to_file(file_path, content):
+    with open(file_path, "w") as f:
+        f.write(content)
 
 def noteArrayToDataFrame(note_array):
     """
@@ -50,12 +57,31 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Generate music using a trained model')
     parser.add_argument('--model-dir', type=str, default='model', help='Directory to load model')
     parser.add_argument('--output-path', type=str, default='output.mid', help='Path to save generated music')
+    parser.add_argument('--config-path', type=str, default='config.json', help='Path to the config file')
     return parser.parse_args()
 
 
 def load_model_from_ckpt(ckpt):
     return tf.keras.models.load_model(ckpt)
 
+def load_stateful_model(config, model):
+    rnn_units = config["rnn_units"]
+    n_vocab = config["n_vocab"]
+    embedding_dim = config["embedding_dim"]
+    sequence_length = config["seq_length"]
+
+    stateful_model = tf.keras.Sequential([
+        tf.keras.layers.InputLayer(batch_input_shape=(1,sequence_length,)),
+        tf.keras.layers.Embedding(input_dim=MELODY_SIZE, output_dim=embedding_dim, input_length=sequence_length),
+        tf.keras.layers.LSTM(units = rnn_units, return_sequences=True, stateful=True),
+        tf.keras.layers.LSTM(units = rnn_units, return_sequences=True, stateful=True),
+        tf.keras.layers.LSTM(units = rnn_units, stateful=True),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(n_vocab)
+    ])
+    stateful_model.compile(loss= tf.losses.SparseCategoricalCrossentropy(from_logits=True), optimizer='adam', metrics=['accuracy'])
+    stateful_model.set_weights(model.get_weights()) 
+    return stateful_model
 
 
 def generate_melody(input_notes, vocab, model, seq_length = SEQUENCE_LENGTH, to_generate = 10):
@@ -67,7 +93,41 @@ def generate_melody(input_notes, vocab, model, seq_length = SEQUENCE_LENGTH, to_
     temperature = 1.0
     for i in range(to_generate):
         prediction_input = np.array(input_notes).reshape(1, seq_length)
+
+        activations = get_activations(model, prediction_input, auto_compile=True)
+        [print(k, '->', v.shape, '- Numpy array') for (k, v) in activations.items()]
+        print(activations['lstm_1'])
+
         prediction_logits = model.predict(prediction_input)
+
+        # print(prediction_logits)
+
+        prediction_logits = prediction_logits / temperature
+
+        predicted_ids = tf.random.categorical(prediction_logits, num_samples=1)
+        prediction = tf.squeeze(predicted_ids, axis=-1)
+
+        prediction_probs = tf.keras.activations.softmax(tf.convert_to_tensor(prediction_logits))
+        print(prediction[0], prediction_probs[0][prediction[0]])
+
+        prediction_output.append(vocab[prediction[0]])
+        input_notes = input_notes[1:] + [vocab[prediction[0]]]
+
+    return prediction_output
+    
+def generate_melody_stateful(input_notes, vocab, model, seq_length = SEQUENCE_LENGTH, to_generate = 10):
+    input_notes = input_notes[-seq_length: ]
+    for i in range(seq_length - len(input_notes)):
+        input_notes.insert(0, np.random.choice(vocab))
+    prediction_output = []
+
+    temperature = 1.0
+    for i in range(to_generate):
+        print(input_notes)
+        prediction_input = np.array(input_notes).reshape(1, len(input_notes))
+        prediction_logits = model.predict(prediction_input)
+
+        # print(prediction_logits)
 
         prediction_logits = prediction_logits / temperature
 
@@ -76,10 +136,22 @@ def generate_melody(input_notes, vocab, model, seq_length = SEQUENCE_LENGTH, to_
         # print(prediction)
 
         prediction_output.append(vocab[prediction[0]])
-        input_notes = input_notes[1:] + [vocab[prediction[0]]]
+        input_notes = [vocab[prediction[0]]]
 
     return prediction_output
+
+def export_weights(model):
+    weight_np = model.get_weights()
     
+    for idx, layer in enumerate(weight_np):
+        print(layer.shape)
+        write_to_file(f"model_weight_{idx:02}.txt", str(layer))
+        # for weight_group in layer:
+        #     flatten = weight_group.reshape(-1).tolist()
+        #     # print("flattened length: ", len(flatten))
+        #     for i in flatten:
+        #         weight_bytes.extend(struct.pack("@f", float(i)))
+
 def create_midi(prediction_output, output_file='test_output.mid'):
     """ convert the output from the prediction to notes and create a midi file
         from the notes """
@@ -102,12 +174,25 @@ def create_midi(prediction_output, output_file='test_output.mid'):
 if __name__ == '__main__':
     args = parse_args()
     model_dir = args.model_dir
+    config_path = args.config_path
+    with open(config_path, 'r') as f:
+        config = json.load(f)
     checkpoint_path = os.path.join(model_dir, 'model.h5')
     model_config_path = os.path.join(model_dir, 'model.json')
     model = load_model_from_ckpt(checkpoint_path)
-    model.summary()
+
     with open(model_config_path, 'r') as f:
         vocab = json.load(f)["vocabulary"]
-    input_notes = [68, 67, 25, 78, 35]
-    melody = generate_melody(input_notes, vocab, model, to_generate= 100)
+    config["n_vocab"] = len(vocab)
+
+    stateful_model = load_stateful_model(config, model)
+    stateful_model.summary()
+
+    export_weights(model)
+
+    input_notes = [68]
+    # input_notes = [68, 129, 30, 31, 32, 35, 129, 37]
+    # input_notes = [68, 67, 25, 78, 35, 68, 67, 25, 78, 35, 68, 67, 25, 78, 35, 68, 67, 25, 78, 35, 68, 67, 25, 78, 35, 68, 67, 25, 78, 35, 68, 67, 25, 78, 35, 68, 67, 25, 78, 35]
+    # melody = generate_melody_stateful(input_notes, vocab, stateful_model, to_generate= 100)
+    melody = generate_melody(input_notes, vocab, model, seq_length=len(input_notes), to_generate= 1)
     create_midi(melody, output_file= args.output_path)
