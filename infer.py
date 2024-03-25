75044df1151f3b68cdbff3278012b04aedac50ce
JamesNBC <1026@newbitcoincity.com>
2024-03-25 19:51:46 +0700

    change infer

--- infer.py
+++ infer.py
@@ -14,7 +14,7 @@ MELODY_NO_EVENT = 129  # (no change from previous event)
 # This can encode monophonic music only.
 MELODY_SIZE = 130
 
-SEQUENCE_LENGTH = 40
+SEQUENCE_LENGTH = 10
 
 def noteArrayToDataFrame(note_array):
     """
@@ -66,7 +66,7 @@ def generate_melody(input_notes, vocab, model, seq_length = SEQUENCE_LENGTH, to_
 
     temperature = 1.0
     for i in range(to_generate):
-        prediction_input = np.array(input_notes).reshape(1, seq_length)
+        prediction_input = np.array(input_notes).reshape(1, seq_length, 1) / MELODY_SIZE
         prediction_logits = model.predict(prediction_input)
 
         prediction_logits = prediction_logits / temperature
