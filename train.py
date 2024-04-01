9a33af27591584bdf46d742c560dae9fa145f04b
JamesNBC <1026@newbitcoincity.com>
2024-04-01 16:06:02 +0700

    remove redundant file

--- train.py
+++ train.py
@@ -11,7 +11,21 @@ import base64
 import json
 import pickle
 import argparse
-from utils import read_notes_from_midi
+from music21 import converter, note, chord, instrument
+
+
+def read_notes_from_midi(file):
+    notes = []
+    midi = converter.parse(file)
+    songs = instrument.partitionByInstrument(midi)
+    for part in songs.parts:
+        pick = part.recurse()
+        for element in pick:
+            if isinstance(element, note.Note):
+                notes.append(str(element.pitch))
+            elif isinstance(element, chord.Chord):
+                notes.append(".".join(str(n) for n in element.normalOrder))
+    return notes
 
 def parse_args():
     parser = argparse.ArgumentParser("Entry script to launch training")
